50803ff1fd2a137cd0920d9cb84c70038da7d654
Vinoth-Kumar-N <vinothkumarn22it@srishakthi.ac.in>
2024-10-10 13:22:42 +0530

    configured backend deployed url

--- MImp-Client/src/services/api.js
+++ MImp-Client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = 'http://localhost:1111';
+const API = 'https://tp-backend-x5wa.onrender.com';
 
 const RegisUser = (data) => axios.post(`${API}/user/register`, data);
 const LoginUser = (data) => axios.post(`${API}/user/login`, data);
